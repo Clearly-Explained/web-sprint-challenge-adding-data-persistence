69257e2bea4a97ecfcb31c7d0db3315417f4d119
Dionne <mylife33@hotmail.com>
2022-11-27 11:30:07 -0600

    MVP

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,21 @@
-// build your `/api/resources` router here
+const router = require('express').Router();
+const db = require('./model');
+
+router.get('/', (req, res, next) => {
+    db.getAll()
+        .then(resource=> {
+            res.status(200).json(resource);
+        })
+        .catch(next)
+})
+
+
+router.post('/', (req, res, next) => {
+    db.add(req.body)
+        .then(newResource => {
+            res.status(201).json(newResource[newResource.length -1])
+        })
+        .catch(next)
+})
+
+module.exports = router;
